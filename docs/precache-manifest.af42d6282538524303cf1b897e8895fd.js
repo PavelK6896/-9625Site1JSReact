0af795385871cade8769dacec6050665fd3d4fa1
PavelK6896 <79208776896@yandex.ru>
2021-08-26 21:41:21 +0300

    update docs

---
+++ docs/precache-manifest.af42d6282538524303cf1b897e8895fd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72cb5370b286502cd3db08c7aa83fc39",
+    "url": "/9625Site1JSReact/index.html"
+  },
+  {
+    "revision": "f7ef6d25cf65296aaec4",
+    "url": "/9625Site1JSReact/static/css/main.4fd425ec.chunk.css"
+  },
+  {
+    "revision": "288a0669b4dd0068e111",
+    "url": "/9625Site1JSReact/static/js/2.5af980e9.chunk.js"
+  },
+  {
+    "revision": "33490309eac47256b827830c802fd4a5",
+    "url": "/9625Site1JSReact/static/js/2.5af980e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7ef6d25cf65296aaec4",
+    "url": "/9625Site1JSReact/static/js/main.d6e79e31.chunk.js"
+  },
+  {
+    "revision": "0db6c271518566ff1359",
+    "url": "/9625Site1JSReact/static/js/runtime-main.d52104ed.js"
+  },
+  {
+    "revision": "96b3826aed556e3b55bc8b686f5f77db",
+    "url": "/9625Site1JSReact/static/media/read1.96b3826a.png"
+  },
+  {
+    "revision": "56a923ae3de58a587cb2fabfdd5d2ce8",
+    "url": "/9625Site1JSReact/static/media/storage2.56a923ae.png"
+  }
+]);
